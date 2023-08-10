5059db385e6b77913922903b0eb4d9580e2a9758
Trang Luong <luongngoctrang0105@gmail.com>
2023-08-10 22:06:20 +0700

    System design initial

---
+++ systemdesign/xUnitTest/Controllers/WeatherForcastControlerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using systemdesign;
+using systemdesign.Controllers;
+
+namespace xUnitTest;
+
+public class WeatherForcastControlerTest
+{
+    [Fact]
+    public void Get_CheckTypeIsWeatherForecastArray()
+    {
+        // Arrange
+        var controller = new WeatherForecastController();
+
+        // Act
+        var result = controller.Get();
+
+        // Assert
+        Assert.IsType<WeatherForecast[]>(result);
+
+    }
+}
---
+++ systemdesign/xUnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace xUnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
